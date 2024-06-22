ecf832c3e7658f737a10b82cf94de1efb9b5f323
basit <hassanbasit1990@gmail.com>
2024-06-22 17:57:45 +0100

    Added changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 
 const Footer = () => {
+  const links = [
+    "The game",
+    "Tournament",
+    "Story",
+    "Variant and Weapons",
+    "Blogs",
+    "Privacy Policy",
+    "About Us",
+  ];
   return (
-    <div className="bg-black px-[60px] w-full pb-[50px]">
-      <footer className="relative w-full min-h-[400px] rounded-[20px]">
-        <div className="absolute right-0 top-0 h-full w-[50px] flex flex-col bg-[black]">
-          <div className="w-full relative h-[55%] rounded-r-[20px] bg-[#8062ff]"></div>
-          <div className="w-full h-[45%] rounded-tl-[20px] bg-black" />
+    <div className="px-[60px] w-full pb-[50px]">
+      <footer className="relative w-full rounded-[20px]">
+        <div className="p-[20px] pb-[60px] w-full h-full flex flex-col gap-[15px]">
+          {links.map((link, id) => (
+            <div key={id} className="flex items-center gap-[20px] group">
+              <div className="hidden mt-[5px] group-hover:block h-[10px] w-[25px] bg-white"></div>
+              <a
+                href="#"
+                className="uppercase font-[PilatExtended-Bold] text-[32px] leading-[1]"
+              >
+                {link}
+              </a>
+            </div>
+          ))}
+        </div>
+
+        <div className="absolute origin-left rotate-[-90deg] right-[-175px] bottom-[-30px] h-[60px] w-[200px] bg-black grid place-items-center">
+          <button className="whitespace-nowrap font-Montserrat font-[600] text-[22px]">
+            👉 UP TO TOP
+          </button>
         </div>
       </footer>
     </div>
