2f4b988edde6574f940057532f2497640af14722
basit <hassanbasit1990@gmail.com>
2024-07-01 23:06:41 +0100

    Added changes

--- src/components/CoverPage.jsx
+++ src/components/CoverPage.jsx
@@ -1,25 +1,26 @@
-import React from "react";
-import { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-
-gsap.registerPlugin(useGSAP);
 
 const CoverPage = () => {
   const container = useRef();
 
-  useGSAP(
-    () => {
-      gsap.to(".text", { opacity: 0, delay: 1 });
-      gsap.to(".cover", { height: 0, stagger: 0.2, duration: 1, delay: 1 });
-    },
-    { scope: container }
-  );
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.to(".text", { opacity: 0, delay: 1 })
+      .to(".cover", {
+        height: 0,
+        stagger: 0.2,
+        duration: 1,
+        delay: -0.8, // Start this animation 0.8 seconds before the previous one ends
+      })
+      .to(".main", { height: 0, duration: 0.1 }, "-=0.2"); // Start this animation 0.2 seconds before the previous one ends
+  }, []);
 
   return (
     <div
       ref={container}
-      className="fixed z-[200] top-0 left-0 w-full h-screen flex"
+      className="main fixed z-[200] top-0 left-0 w-full h-screen flex"
     >
       <div className="cover w-[20%] h-full bg-[#000513]" />
       <div className="cover w-[20%] h-full bg-[#000513]" />
